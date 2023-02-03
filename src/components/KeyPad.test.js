9c1c40822cff35507c7caa5873c07f8bba14122a
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2023-02-03 13:02:45 -0800

    Resolve #14 Add elements for nums 0-9
    - test the same
    - Resolves #14

--- src/components/KeyPad.test.js
+++ src/components/KeyPad.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import KeyPad from './KeyPad'
+import { NumberStringMap as numbers } from '../util/constants'
 
 describe('KeyPad', () => {
   it('has a clickable equals button', () => {
@@ -7,5 +8,13 @@ describe('KeyPad', () => {
     const equals = screen.getByText('=')
     expect(equals.id).toBe('equals')
     expect(equals.className).toBe('equals')
+  })
+
+  it('has an element for each number 0-9', () => {
+    render(<KeyPad />)
+    Object.keys(numbers).forEach((number) => {
+      const numberElement = screen.getByText(number)
+      expect(numberElement.id).toBe(numbers[number])
+    })
   })
 })
---
+++ src/util/constants.js
@@ -0,0 +1,14 @@
+export const NumberStringMap = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine',
+}
+
+export const NumPadRows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]]
