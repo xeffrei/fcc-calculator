5f6a21cdc6c7f03b90811bb34a91972d4b810c87
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2023-02-10 08:33:52 -0800

    Test issues #13 #16 #17

--- src/components/KeyPad.test.js
+++ src/components/KeyPad.test.js
@@ -1,20 +1,21 @@
 import { render, screen } from '@testing-library/react'
 import KeyPad from './KeyPad'
-import { NumberStringMap as numbers } from '../util/constants'
+import { NumberStringMap as numbers, KeyPadButtons } from '../util/constants'
 
 describe('KeyPad', () => {
-  it('has a clickable equals button', () => {
-    render(<KeyPad />)
-    const equals = screen.getByText('=')
-    expect(equals.id).toBe('equals')
-    expect(equals.className).toBe('equals')
-  })
-
   it('has an element for each number 0-9', () => {
     render(<KeyPad />)
     Object.keys(numbers).forEach((number) => {
       const numberElement = screen.getByText(number)
       expect(numberElement.id).toBe(numbers[number])
+    })
+  })
+
+  it('has elements for each key pad button', () => {
+    render(<KeyPad />)
+    Object.keys(KeyPadButtons).forEach((button) => {
+      const buttonElement = screen.getByText(KeyPadButtons[button].value)
+      expect(buttonElement.id).toBe(KeyPadButtons[button].id)
     })
   })
 })
